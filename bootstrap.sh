bfdaa6811ee1420fd2ffd0224a15562abae5100b
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2019-03-22 12:45:28 +0100

    simplified bootstrapping script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,10 @@
 #! /usr/bin/env bash
 
 cd `dirname .`
-#sbt assembly
-
-rm -rv tmp; mkdir tmp
-cd tmp
-ln -s ../deps .
-java -jar ../ogss.jar build ../src/main/spec/oil.skill -L Java --package ogss.oil -d ../lib
-cd ..
 
 rm -rv src/main/java/ogss/oil
-cp -rv tmp/ogss src/main/java
-rm -rv tmp
+
+java -jar ogss.jar build src/main/spec/oil.skill -L Java --package ogss.oil -o src/main/java -d lib
 
 sbt assembly
 cp target/scala*/ogss.jar .
