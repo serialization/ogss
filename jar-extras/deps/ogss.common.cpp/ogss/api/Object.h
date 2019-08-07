6b52e6217236a8c7bd43e2e952af275a3a8ea9a4
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2019-08-07 16:40:33 +0200

    [c++] add a virtual destructor to Object to allow users to define non-trivial destructors

--- jar-extras/deps/ogss.common.cpp/ogss/api/Object.h
+++ jar-extras/deps/ogss.common.cpp/ogss/api/Object.h
@@ -2,91 +2,90 @@
 // Created by Timm Felden on 06.11.15.
 //
 
-#ifndef SKILL_CPP_COMMON_OBJECT_H
-#define SKILL_CPP_COMMON_OBJECT_H
+#ifndef OGSS_COMMON_OBJECT_H
+#define OGSS_COMMON_OBJECT_H
 
 #include "String.h"
 
 namespace ogss {
-    namespace internal {
-        class DistributedField;
+namespace internal {
+class DistributedField;
 
-        class FieldDeclaration;
+class FieldDeclaration;
 
-        class LazyField;
+class LazyField;
 
-        class AbstractPool;
+class AbstractPool;
 
-        template<class T>
-        class Pool;
+template <class T> class Pool;
 
-        class Writer;
-    }
-    namespace fieldTypes {
-        class AnyRefType;
-    }
-    namespace api {
-        class File;
-
-        /**
-         * an abstract skill object, i.e. the type of an annotation and the base type of
-         * every known and unknown skill type instance.
-         */
-        class Object {
-
-        protected:
-            ObjectID id;
-
-            //! bulk allocation
-            Object() {};
-
-        public:
-            /**
-             * @return the static type ID
-             * @note types with stid == -1 are named and hold a pointer to their pool
-             */
-            virtual TypeID stid() const = 0;
-
-            /**
-             * query, whether the object is marked for deletion and will be destroyed on flush
-             */
-            bool isDeleted() const { return 0 == id; }
-
-            friend class internal::AbstractPool;
-
-            template<class T>
-            friend
-            class internal::Pool;
-
-            template<typename T>
-            friend
-            struct std::hash;
-
-            friend class internal::FieldDeclaration;
-
-            friend class internal::DistributedField;
-
-            friend class internal::LazyField;
-
-            friend class internal::Writer;
-
-            friend class fieldTypes::AnyRefType;
-
-            friend class File;
-        };
-
-        /**
-         * An Obj that holds a pointer to its pool.
-         *
-         * @author Timm Felden
-         * @note This type definition is in internal, because we have to protect the user from tampering with ID
-         */
-        struct NamedObj {
-            explicit NamedObj(const internal::AbstractPool *pool) : pool(pool) {}
-
-            const internal::AbstractPool *const pool;
-        };
-    }
+class Writer;
+} // namespace internal
+namespace fieldTypes {
+class AnyRefType;
 }
+namespace api {
+class File;
 
-#endif //SKILL_CPP_COMMON_OBJECT_H
+/**
+ * an abstract skill object, i.e. the type of an annotation and the base type of
+ * every known and unknown skill type instance.
+ */
+class Object {
+
+  protected:
+    ObjectID id;
+
+    //! bulk allocation
+    Object(){};
+
+    virtual ~Object() = default;
+
+  public:
+    /**
+     * @return the static type ID
+     * @note types with stid == -1 are named and hold a pointer to their pool
+     */
+    virtual TypeID stid() const = 0;
+
+    /**
+     * query, whether the object is marked for deletion and will be destroyed on
+     * flush
+     */
+    bool isDeleted() const { return 0 == id; }
+
+    friend class internal::AbstractPool;
+
+    template <class T> friend class internal::Pool;
+
+    template <typename T> friend struct std::hash;
+
+    friend class internal::FieldDeclaration;
+
+    friend class internal::DistributedField;
+
+    friend class internal::LazyField;
+
+    friend class internal::Writer;
+
+    friend class fieldTypes::AnyRefType;
+
+    friend class File;
+};
+
+/**
+ * An Obj that holds a pointer to its pool.
+ *
+ * @author Timm Felden
+ * @note This type definition is in internal, because we have to protect the
+ * user from tampering with ID
+ */
+struct NamedObj {
+    explicit NamedObj(const internal::AbstractPool *pool) : pool(pool) {}
+
+    const internal::AbstractPool *const pool;
+};
+} // namespace api
+} // namespace ogss
+
+#endif // OGSS_COMMON_OBJECT_H
